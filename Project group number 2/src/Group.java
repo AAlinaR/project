b540759309597df6dd28200079248b78ac26ef5b
Армян <@DESKTOP-A36A4BR>
2019-02-17 10:11:30 +0300

    Git tutorial group2

---
+++ Project group number 2/src/Group.java
@@ -0,0 +1,11 @@
+
+public class Group {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Группа №2: Ахатова А.Р, Акмалиева А.А, Мугафарова З.Н, Сиротина А.А, Султанов А.,Яфаева Л.");
+		
+
+	}
+
+}
