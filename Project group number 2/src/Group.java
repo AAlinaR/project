c053b0c735b7c1eb43f1fd4b9d984d41f5742d25
AAlinaR <47585443+AAlinaR@users.noreply.github.com>
2019-02-18 10:36:03 +0300

    Update Group.java

--- Project group number 2/src/Group.java
+++ Project group number 2/src/Group.java
@@ -3,7 +3,13 @@ public class Group {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Group number 2: Akhatova A.R., Akmalieva A.A., Mugafarova Z.N., Sirotina A.A., Sultanov A., Yafaeva L..");
+		System.out.println("Group number 2:");
+		System.out.println("1. Akhatova A.R.");
+				System.out.println("2. Akmalieva A.A.");
+						System.out.println("3. Mugafarova Z.N.");
+						System.out.println("4. Sirotina A.A.");
+						System.out.println("5. Sultanov A.A");
+						System.out.println("6. Yafaeva L.Z");
 		
 
 	}
