eb8aa44a8b7c8cb72aba06d88ce2af758f1fd35c
AAlinaR <47585443+AAlinaR@users.noreply.github.com>
2019-02-18 10:54:47 +0300

    Update Group.java

--- Project group number 2/src/Group.java
+++ Project group number 2/src/Group.java
@@ -1,17 +1,21 @@
 
 public class Group {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		/* Это коллективная java-программа.
+   В результате выполнения на экран будет выведено 'список группы №2'
+   */
 	
-		System.out.println("Group number 2:");
-		System.out.println("1. Akhatova A.R.");
+		System.out.println("Group number 2:"); /* Вывод сообщения на экран
+   */
+		                System.out.println("1. Akhatova A.R.");
 				System.out.println("2. Akmalieva A.A.");
-						System.out.println("3. Mugafarova Z.N.");
-						System.out.println("4. Sirotina A.A.");
-						System.out.println("5. Sultanov A.A");
-						System.out.println("6. Yafaeva L.Z");
+				System.out.println("3. Mugafarova Z.N.");
+				System.out.println("4. Sirotina A.A.");
+				System.out.println("5. Sultanov A.A");
+				System.out.println("6. Yafaeva L.Z");
 		
-
+/* Тело метода main содержит семь строк */
 	}
 
 }
