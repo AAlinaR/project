60a85b96d57518e1b971e37086fc5ecb1c587ca2
LiliyaZ <47584201+LiliyaZ@users.noreply.github.com>
2019-04-01 11:04:16 +0500

    Update Group.java

--- Project group number 2/src/Group.java
+++ Project group number 2/src/Group.java
@@ -13,7 +13,8 @@ public class Group {
 				System.out.println("3. Mugafarova Z.N.");
 				System.out.println("4. Sirotina A.A.");
 				System.out.println("5. Sultanov A.A");
-				System.out.println("6. Yafaeva Lily")
+				System.out.println("6. Yafaeva L.Z");
+		System.out.println("Hello")
 		
 /* Тело метода main содержит семь строк */
 	}
