f03fff1eed3137cd498a04c4b74e96c55d359818
Zenfirochka <47803778+Zenfirochka@users.noreply.github.com>
2019-04-02 23:29:20 +0500

    Update Group.java

--- Project group number 2/src/Group.java
+++ Project group number 2/src/Group.java
@@ -15,7 +15,7 @@ public class Group {
 				System.out.println("5. Sultanov A.A");
 				System.out.println("6. Yafaeva Lily")
 					System.out.println("Ассаламу аллейкум Алена здэс")
-		
+		System.out.println("Ассаламу аллейкум Зенфира здэс")
 /* Тело метода main содержит семь строк */
 	}
 
