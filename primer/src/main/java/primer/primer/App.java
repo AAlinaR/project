7be89f65488024f3a901c6ec695edc468343cec7
Армян <@DESKTOP-A36A4BR>
2019-04-02 19:19:36 +0300

    Maven Project

--- primer/src/main/java/primer/primer/App.java
+++ primer/src/main/java/primer/primer/App.java
@@ -1,15 +1,19 @@
+package primer.primer;
 
-public class Group {
-
-	public static void main(String[] args) {
-	
-		System.out.println("Group number 2:");
+/**
+ * Spisok!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Group number 2:");
 		System.out.println("1. Akhatova A.R.");
 				System.out.println("2. Akmalieva A.A.");
 						System.out.println("3. Mugafarova Z.N.");
 						System.out.println("4. Sirotina A.A.");
 						System.out.println("5. Sultanov A.A");
 						System.out.println("6. Yafaeva L.Z");
-						
-	}
+    }
 }
